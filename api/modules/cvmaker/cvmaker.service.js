9db1cea319a1fd43be87a3a0116e42cf74148f20
Nikola <nikolajovanovic5556@gmail.com>
2021-10-29 15:06:33 +0200

    api

--- api/modules/cvmaker/cvmaker.service.js
+++ api/modules/cvmaker/cvmaker.service.js
@@ -1,28 +1,59 @@
 import pdfGen from 'html-pdf';
 import pdfPage from 'pdf-page-counter';
+import { PDFDocument } from 'pdf-lib';
 import fs from 'fs';
+
 import findTemplate from './templates';
 
+// ------------------------------
+// ------------------------------
+// utils
+// ------------------------------
+// ------------------------------
+
 const PDFS_LEFT = ['template4', 'template5', 'template6', 'template8', 'template9'];
+
+const countPages1 = (buffer) => {};
+const countPages2 = async (buffer) => {
+    const pdf = await PDFDocument.load(buffer);
+    return pdf.getPageCount();
+};
+
+// ------------------------------
+// ------------------------------
+// Buffer
+// ------------------------------
+// ------------------------------
 
 export const createPdf = (name, params, img_file, res) => {
     const { template: template1 } = findTemplate(name, params, img_file, 1);
 
-    pdfGen.create(template1, {}).toBuffer((err, buff) => {
+    pdfGen.create(template1, {}).toBuffer(async (err, buff) => {
         if (err) res.status(400).send('Error creating pdf!');
         const cv1Buffer = buff;
 
         if (PDFS_LEFT.includes(name)) {
-            pdfPage(cv1Buffer).then((pdfData) => {
-                console.log(pdfData.numpages);
-                const { template: template2 } = findTemplate(name, params, img_file, pdfData.numpages);
+            const noOfPages = await countPages2(cv1Buffer);
+            console.log(noOfPages);
 
-                pdfGen.create(template2, {}).toBuffer((err, buff) => {
-                    if (err) res.status(400).send('Error creating pdf!');
-                    const cv2Buffer = buff;
-                    res.status(201).send(cv2Buffer.toString('base64'));
-                });
+            const { template: template2 } = findTemplate(name, params, img_file, noOfPages);
+
+            pdfGen.create(template2, {}).toBuffer((err, buff) => {
+                if (err) res.status(400).send('Error creating pdf!');
+                const cv2Buffer = buff;
+                res.status(201).send(cv2Buffer.toString('base64'));
             });
+
+            // pdfPage(cv1Buffer).then((pdfData) => {
+            //     console.log(pdfData.numpages);
+            //     const { template: template2 } = findTemplate(name, params, img_file, pdfData.numpages);
+
+            //     pdfGen.create(template2, {}).toBuffer((err, buff) => {
+            //         if (err) res.status(400).send('Error creating pdf!');
+            //         const cv2Buffer = buff;
+            //         res.status(201).send(cv2Buffer.toString('base64'));
+            //     });
+            // });
         } else {
             res.status(201).send(cv1Buffer.toString('base64'));
         }
@@ -38,21 +69,30 @@ export const createPdf = (name, params, img_file, res) => {
 export const createPdfFile = (name, params, img_file, res) => {
     const { fileName: fn1, template: template1 } = findTemplate(name, params, img_file, 1);
 
-    pdfGen.create(template1, {}).toFile(`documents/${fn1}.pdf`, (err, pdf) => {
+    pdfGen.create(template1, {}).toFile(`documents/${fn1}.pdf`, async (err, pdf) => {
         if (err) res.status(400).send('Error creating pdf!');
 
         if (PDFS_LEFT.includes(name)) {
             let cv1Buffer = fs.readFileSync(pdf.filename);
+            const noOfPages = await countPages2(cv1Buffer);
 
-            pdfPage(cv1Buffer).then((pdfData) => {
-                console.log(pdfData.numpages);
-                const { fileName: fn2, template: template2 } = findTemplate(name, params, img_file, pdfData.numpages);
+            console.log(noOfPages);
+            const { fileName: fn2, template: template2 } = findTemplate(name, params, img_file, noOfPages);
 
-                pdfGen.create(template2, {}).toFile(`documents/${fn2}.pdf`, (err, pdf) => {
-                    if (err) res.status(400).send('Error creating pdf!');
-                    res.status(201).sendFile(pdf.filename);
-                });
+            pdfGen.create(template2, {}).toFile(`documents/${fn2}.pdf`, (err, pdf) => {
+                if (err) res.status(400).send('Error creating pdf!');
+                res.status(201).sendFile(pdf.filename);
             });
+
+            //    pdfPage(cv1Buffer).then((pdfData) => {
+            //         console.log(pdfData.numpages);
+            //         const { fileName: fn2, template: template2 } = findTemplate(name, params, img_file, pdfData.numpages);
+
+            //         pdfGen.create(template2, {}).toFile(`documents/${fn2}.pdf`, (err, pdf) => {
+            //             if (err) res.status(400).send('Error creating pdf!');
+            //             res.status(201).sendFile(pdf.filename);
+            //         });
+            //     });
         } else {
             res.status(201).sendFile(pdf.filename);
         }
