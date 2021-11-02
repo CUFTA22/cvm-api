5bd174a3b0a8741948ad9358a49ef1c643e7d588
Nikola <nikolajovanovic5556@gmail.com>
2021-11-02 22:24:05 +0100

    api

--- api/jobs/job_dbCleanup.js
+++ api/jobs/job_dbCleanup.js
@@ -1,20 +1,20 @@
-import { getCVForCleanup, deleteCVById } from '../modules/cvmaker/cvmaker.service';
+// import { getCVForCleanup, deleteCVById } from '../modules/cvmaker/cvmaker.service';
 
-const oneDay = 1000 * 60 * 60 * 24;
-const expirationTime = oneDay * 30 * 3; // 3 months, last digit = number of months
+// const oneDay = 1000 * 60 * 60 * 24;
+// const expirationTime = oneDay * 30 * 3; // 3 months, last digit = number of months
 
-const dbCleanupJob = async () => {
-    try {
-        const lt = new Date(Date.now() - expirationTime);
+// const dbCleanupJob = async () => {
+//     try {
+//         const lt = new Date(Date.now() - expirationTime);
 
-        const cvs = await getCVForCleanup(lt);
+//         const cvs = await getCVForCleanup(lt);
 
-        for (const cv of cvs) {
-            await deleteCVById(cv._id);
-        }
-    } catch (err) {
-        console.error('Error     | DB Cleanup');
-    }
-};
+//         for (const cv of cvs) {
+//             await deleteCVById(cv._id);
+//         }
+//     } catch (err) {
+//         console.error('Error     | DB Cleanup');
+//     }
+// };
 
-export default dbCleanupJob;
+// export default dbCleanupJob;
